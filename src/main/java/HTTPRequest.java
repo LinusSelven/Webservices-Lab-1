f73984791c6e30b74fad31e13f93d9596a094383
Skoorps <kpOC8&1I3$Js&03*bWN8>
2020-02-20 21:16:57 +0100

    cleaning up some more

--- src/main/java/HTTPRequest.java
+++ src/main/java/HTTPRequest.java
@@ -1,6 +1,3 @@
-import com.google.gson.JsonObject;
-import org.json.JSONObject;
-
 
 import java.util.HashMap;
 
@@ -12,44 +9,23 @@ public class HTTPRequest {
 
     private HashMap<String, String> headers = new HashMap<String, String>();
 
-    private JsonObject body;
-
-    public void TheRequest() {
-    }
-
-    public String getStartLineStatus() {
-        return StartLineStatus;
-    }
 
     public void setStartLineStatus(String startLineStatus) {
         StartLineStatus = startLineStatus;
     }
 
-    public void setHeaders(String key, String value) {
-        this.headers.put(key, value);
-    }
 
     public HashMap<String, String> getHeaders() {
         return headers;
     }
 
-    public String getStartLineImplementation() {
-        return StartLineImplementation;
-    }
 
     public void setStartLineImplementation(String startLineImplementation) {
         StartLineImplementation = startLineImplementation;
-    }
-
-    public String getStartLineURL() {
-        return StartLineURL;
     }
 
     public void setStartLineURL(String startLineURL) {
         StartLineURL = startLineURL;
     }
 
-    public void setJsonObject(JsonObject jsonObject) {
-        body = jsonObject;
-    }
 }
